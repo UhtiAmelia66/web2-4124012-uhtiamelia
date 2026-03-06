9ba1ec369bf602880d030791f8eba355610be15a
yazkaazahaa <yazkaazahaa5@gmail.com>
2026-03-06 10:44:00 +0700

    feat: tambah perkenalan dua anggota

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
     return view('welcome');
+
 });
 
+// Route baru – wajib ditambahkan!
 Route::get('/perkenalan', function () {
 
     return '<h1>Halo! Nama saya Uhti Amelia</h1>
 
             <p>NIM: 4124012 | Prodi: Sistem Informasi</p>
 
+            <p>Saya siap belajar Laravel! 🚀</p>
+
+            <p>------------------------------------</p>
+
+            <h1>Halo! Nama saya Yazkaa Zahaa</h1>
+
+            <p>NIM: 4124013 | Prodi: Sistem Informasi</p>
+
             <p>Saya siap belajar Laravel! 🚀</p>';
 
-});
-
-Route::get('/yazkaa', function () {
-    return '<h1>Halo! Nama saya Yazkaa Zahaa</h1>
-            <p>NIM: 4124013 | Prodi: Sistem Informasi</p>
-            <p>Saya siap belajar Laravel!</p>';
 });
